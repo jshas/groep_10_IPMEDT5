0d97c52af7a4b5272d39234f4ae4a294bf04fd29
jshas <62602628+jshas@users.noreply.github.com>
2022-03-22 11:45:41 +0100

    Basis main.css en baseview aangemaakt

---
+++ resources/views/ baseview.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <title>@yield('title')</title>
+    {{-- Voeg: @section('title','De gewenste titel') toe aan het extend bestand om een een titel mee te geven in de extend. --}}
+</head>
+    @yield('body')
+</html>
