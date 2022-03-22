0a17719ff716cf524b9014897d44639f83e4f859
TimovandenBerg <mrtimogames@gmail.com>
2022-03-22 12:01:13 +0100

    Sms controller en een route aangemaakt. Sms sturen werkt.

---
+++ app/Http/Controllers/SmsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Nexmo\Laravel\Facade\Nexmo;
+
+class SmsController extends Controller
+{
+    public function index()
+    {
+        Nexmo::message()->send([
+            'to' => '31613013763',
+            'from' => '31613013763',
+            'text' => 'Test SMS'
+        ]);
+
+        echo 'Message sent';
+    }
+}
