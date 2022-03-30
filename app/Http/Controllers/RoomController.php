710ba36ee5682fea214aee87143e1940e6560bc8
jshas <62602628+jshas@users.noreply.github.com>
2022-03-30 13:55:34 +0200

    Added validation and css for creating, and setup for editing rooms.

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -8,24 +8,88 @@ use App\Models\Sensor;
 
 class RoomController extends Controller
 {
-    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(){
         $rooms = Room::with('sensors')->get();
         return view('dashboard', ['rooms' => $rooms]);
     }
     
-    public function show($name){
-        return Room::where('name', $name);
-    }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create(){
         $rooms = Room::with('sensors')->get();
         return view('rooms.create', ['rooms' => $rooms]);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request, Room $room){
-        $room-> name = $request->input('name');
+        $validated = $request->validate([
+            'name' => 'required|unique:rooms|max:40',
+        ]);
+        $room->name = $validated['name'];
         $room->save();
-        return view('rooms.create');
+        return redirect('/rooms/create');
+    }
+
+    /**+
+     * Display the specified resource.
+     *
+     * @param  string  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function show($name){
+        return Room::where('name', $name);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($name)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $name)
+    {
+        $newRoomname = $request->get()->first();
+        $room = Room::where("name", $name);
+        $room->name = $request->name;
+        $room->save();
+        return response();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($name)
+    {
+        //
     }
 }
+
--- resources/views/rooms/create.blade.php
+++ resources/views/rooms/create.blade.php
@@ -1,32 +1,56 @@
 @extends('baseview')
-@section('title', 'Room functions')
+@section('title', 'Create Room')
+@section('subheading', 'Add Room' )
 @include('components.header')
 
 @section('content')
-    <table class="table">
-        <tr class="table__row">
-        <th class="table__header">Current Rooms</th>
-        </tr>
-        @foreach($rooms as $room)
-            <tr>
-                <td class="table__text">{{ $room->name }}</td>
-                <td><button>edit</button></td>
-                <td><button>delete</button></td>
-            </tr>
-        @endforeach
-    </table>
 
-    <form action="/rooms" method="POST">
-        @csrf
+<main class="create-room">
+    <article class="form u-flex-center">
+        <form class="form__form" action="/rooms" method="POST">
+            @csrf
+            <h2 class="form__header">Add New Room </h2>
+            <section class="form__section">
+                <label class="form__label" for="name">Name</label>
+                <input class="form__input" id="name" placeholder="Name of the room." name="name" type="text" value="{{ old('name') }}">
+                @if ($errors->name)
+                <section class="form__alert">
+                    @foreach ($errors->all() as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+                </section>
+                @endif    
+            </section>
+    
+            <section class="form__section">
+                <label class="form__label" for="MQTT-Topic">MQTT Topic</label>
+                <input class="form__input" id="MQTT-Topic" placeholder="" type="text">
+            </section>
+    
+            <section class="form__section">
+                <button class="form__button type="submit">Add Room</button>
+            </section>
+        </form>
 
-        <label for="name">Name</label>
-        <input id="name" placeholder="Name of the room." name="name" type="text">
+            <article class="table  u-flex-center">
+                <table class="table__table">
+                    <tr class="table__row">
+                        <th class="table__header">Current Rooms</th>
+                    </tr>
+                    @foreach($rooms as $room)
+                        <tr class="table__row">
+                            <td class="table__text">{{ $room->name }}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            </article>
+    </article>
 
-        <label for="MQTT-Topic">MQTT Topic</label>
-        <input id="MQTT-Topic" name="Name of the topic. (e.g: "basement")" type="text">
 
-        <button type="submit">Add Room</button>
 
-    </form>
+
+</main>
+
+
 
 @endsection
