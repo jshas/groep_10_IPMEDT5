e1c771edbb8c25d6b7024e0f7580dab066d9cc09
TimovandenBerg <mrtimogames@gmail.com>
2022-04-06 10:18:39 +0200

    SmsController kijkt nu na hoeveel sms er al zijn verstuurd. Als het onder de 2 is dan zegt ie dat er al genoeg smsjes zijn verstuurd.

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -4,18 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Nexmo\Laravel\Facade\Nexmo;
+use DB;
 
 class SmsController extends Controller
 {
     public function index()
     {
-        // Nexmo::message()->send([
-        //     'to' => '31613013763',
-        //     'from' => '31613013763',
-        //     'text' => 'Brand Gedetecteerd!!!'
-        // ]);
+        // Count voor de sms ophalen
+        $sms_count = \App\Models\SmsCount::first();
 
-        echo 'Message sent';
-        echo "<script>setTimeout(function(){ window.location.href = '/'; }, 5000);</script>";
+        if($sms_count-> count <= 2) {
+            // Nexmo::message()->send([
+            //     'to' => '31613013763',
+            //     'from' => '31613013763',
+            //     'text' => 'Brand Gedetecteerd!!!'
+            // ]);
+            $affected = DB::table('sms_counting')
+              ->update(['count' => $sms_count->count + 1]);
+
+            echo $sms_count->count;
+            echo 'Message sent';
+            echo "<script>setTimeout(function(){ window.location.href = '/'; }, 5000);</script>";
+        } else {
+            echo "Er zijn al genoeg smsjes verstuurd";
+            echo "<script>setTimeout(function(){ window.location.href = '/'; }, 5000);</script>";
+        }
+
+        
     }
 }
