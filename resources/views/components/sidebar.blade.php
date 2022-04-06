a9f485f12ffdb96926657cb28878f802e908f636
jshas <62602628+jshas@users.noreply.github.com>
2022-04-06 11:49:54 +0200

    Fix: .sidenav added <a>

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,20 +1,34 @@
 <nav class="sidenav">
-    <li class="sidenav__items">
-        <figure class="sidenav__figure">
-            <img class="sidenav__img" alt="Icon of a thermometer." src="icon/thermometer.png">
-            <figcaption class="sidenav__text">Sensoren</figcaption>
-        </figure>
-    </li>
-    <li class="sidenav__items">
-        <figure class=sidenav__figure><img class="sidenav__img" alt="Plus sign."src="icon/plus.png"><figcaption class="sidenav__text">Unit toevoegen</figcaption>
-        </figure></li>
-    <li class="sidenav__items">
-        <figure class="sidenav__figure">
-            <img class="sidenav__img" alt="Sync sign."src="icon/sync.png">
-            <figcaption class="sidenav__text">Sync</figcaption> 
-        </figure></li>
-    <li class="sidenav__items">      
-    <figure class="sidenav__figure">
-        <img class="sidenav__img sidenav__img--smaller" alt="Power sign."src="icon/power.png"><figcaption class="sidenav__text">Power opties</figcaption> 
-    </figure></li>
+    <a class="sidenav__link" href="/">
+        <li class="sidenav__items">
+            <figure class="sidenav__figure">
+                <img class="sidenav__img" alt="Icon of a thermometer." src="icon/thermometer.png">
+                <figcaption class="sidenav__text">Sensoren</figcaption>
+            </figure>
+        </li>
+    </a>
+    <a class="sidenav__link" href="/">
+        <li class="sidenav__items">
+            <figure class=sidenav__figure><img class="sidenav__img" alt="Plus sign."src="icon/plus.png">
+                <figcaption class="sidenav__text">Unit toevoegen</figcaption>
+            </figure>
+        </li>
+    </a>
+    <a class="sidenav__link" href="/">
+        <li class="sidenav__items">
+            <figure class="sidenav__figure">
+                <img class="sidenav__img" alt="Sync sign."src="icon/sync.png">
+                <figcaption class="sidenav__text">Sync</figcaption> 
+            </figure>
+        </li>
+    </a>
+
+    <a href="/" class="sidenav__link">
+        <li class="sidenav__items">      
+            <figure class="sidenav__figure">
+                <img class="sidenav__img sidenav__img--smaller" alt="Power sign."src="icon/power.png">
+                <figcaption class="sidenav__text">Power opties</figcaption> 
+            </figure>
+        </li>
+    </a>
 </nav>
