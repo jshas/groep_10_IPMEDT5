b4eb9b8687848a26fd3c28b13ceffdaf93fae510
jshas <62602628+jshas@users.noreply.github.com>
2022-04-06 14:26:18 +0200

    added homepage link to header bar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,8 @@
 <header class="page-header">
-    <figure class="page-header__figure">
-        <img src="/icon/logo.png" alt ="Fire Detection Company Logo" class="page-header__img">
-    </figure>
-    <h1 class="page-header__heading">Fire Detection System</h1>
+    <a class="page-header__link" href="/">
+        <figure class="page-header__figure">
+            <img src="/icon/logo.png" alt ="Fire Detection Company Logo" class="page-header__img">
+        </figure>
+        <h1 class="page-header__heading">Fire Detection System</h1>
+    </a>
 </header>
