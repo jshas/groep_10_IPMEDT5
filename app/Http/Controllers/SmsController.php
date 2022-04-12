57c8881b97711c14e1654b73d7516cd0da348ac3
TimovandenBerg <mrtimogames@gmail.com>
2022-04-12 13:30:52 +0200

    Na 3 smsjes ander bericht. Boven 3 smsjes wordt er niks meer vestuurd.

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -22,11 +22,28 @@ class SmsController extends Controller
             $affected = DB::table('sms_counting')
               ->update(['count' => $sms_count->count + 1]);
 
-            echo $sms_count->count;
-            echo 'Message sent';
+            echo $sms_count->count . ": ";
+            echo "Message sent";
+            echo ". Over 5 seconden wordt u weer terug gestuurd naar de homepagina";
             echo "<script>setTimeout(function(){ window.location.href = '/'; }, 5000);</script>";
-        } else {
+        } 
+        if($sms_count-> count == 3) {
+            // Nexmo::message()->send([
+            //     'to' => '31613013763',
+            //     'from' => '31613013763',
+            //     'text' => 'Er zijn nu al 3 sms'jes verstuurd. Vergeet niet op de reset knop te drukken als je weer sms'jes wilt ontvangen.'
+            // ]);
+            $affected = DB::table('sms_counting')
+              ->update(['count' => $sms_count->count + 1]);
+
+            echo $sms_count->count . ": ";
+            echo 'Message sent';
+            echo ". Over 5 seconden wordt u weer terug gestuurd naar de homepagina";
+            echo "<script>setTimeout(function(){ window.location.href = '/'; }, 5000);</script>";
+        }
+        if($sms_count-> count > 3) {
             echo "Er zijn al genoeg smsjes verstuurd";
+            echo ". Over 5 seconden wordt u weer terug gestuurd naar de homepagina";
             echo "<script>setTimeout(function(){ window.location.href = '/'; }, 5000);</script>";
         }
 
