f85731a450e3adb6f02e4d186579081af155061a
Devin <hengselman@gmail.com>
2022-04-13 16:07:01 +0200

    Added MqttSubscriber

---
+++ MqttTesting.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/shared/config.php';
+require __DIR__ . '/shared/SimpleLogger.php';
+
+use PhpMqtt\Client\Examples\Shared\SimpleLogger;
+use PhpMqtt\Client\Exceptions\MqttClientException;
+use PhpMqtt\Client\MqttClient;
+use Psr\Log\LogLevel;
+
+
+// Create an instance of a PSR-3 compliant logger. For this example, we will also use the logger to log exceptions.
+$logger = new SimpleLogger(LogLevel::INFO);
+
+try {
+    // Create a new instance of an MQTT client and configure it to use the shared broker host and port.
+    $client = new MqttClient(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 'test-subscriber', MqttClient::MQTT_3_1, null, $logger);
+
+    // Connect to the broker without specific connection settings but with a clean session.
+    $client->connect(null, true);
+
+    // Subscribe to the topic 'hengseltest' using QoS 0.
+    $client->subscribe('hengseltest', function (string $topic, string $message, bool $retained) use ($logger, $client) {
+        $logger->info('We received a {typeOfMessage} on topic [{topic}]: {message}', [
+            'topic' => $topic,
+            'message' => $message,
+            'typeOfMessage' => $retained ? 'retained message' : 'message',
+        ]);
+
+        $host = "localhost";
+        $db_user = "laravel";
+        $db_password = "password";
+        $db_name = "laravel";
+
+        $sqlconnect = mysqli_connect($host, $db_user, $db_password, $db_name);
+
+        if($sqlconnect->connect_errno!=0){
+            echo "Error\n";
+        } else {
+
+            echo "Connected with database!\n";
+            $SQL="INSERT INTO temperatuur_sensor VALUES (, 'living_room', $message)";
+
+            if (@mysqli_query($sqlconnect, $SQL)){
+                echo "Added to database.\n";
+            } else{
+                echo "Not added to database.\n";
+            }
+        }
+
+
+        // After receiving the first message on the subscribed topic, we want the client to stop listening for messages.
+        //     $client->interrupt();
+    }, MqttClient::QOS_AT_MOST_ONCE);
+
+    // Since subscribing requires to wait for messages, we need to start the client loop which takes care of receiving,
+    // parsing and delivering messages to the registered callbacks. The loop will run indefinitely, until a message
+    // is received, which will interrupt the loop.
+    $client->loop(true);
+
+    // Gracefully terminate the connection to the broker.
+    $client->disconnect();
+} catch (MqttClientException $e) {
+    // MqttClientException is the base exception of all exceptions in the library. Catching it will catch all MQTT related exceptions.
+    $logger->error('Subscribing to a topic using QoS 0 failed. An exception occurred.', ['exception' => $e]);
+}
